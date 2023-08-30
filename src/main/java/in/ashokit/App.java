160f2f59d308572f273bc9ec0a5458a493d69067
sayyadshoeb <shoebsayyad41@gmail.com>
2023-08-30 19:06:33 +0530

    modified

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello in ashok i1t!" );
+//task=101 changes
+         int i=10;
+         int j=20;
+         int k=i+j;
     }
 }
